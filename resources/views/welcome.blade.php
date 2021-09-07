7da29f13a29db0f7b02b91c2c96abf8e26a8b1d3
lavateam <lava.developers.tm@gmail.com>
2021-09-07 22:40:34 +0200

    Final update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,44 +17,6 @@
         </div>
     </section>
 
-    <div class="container">
-
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@mdo</td>
-
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                    <td>@mdo</td>
-
-                </tr>
-            </tbody>
-        </table>
-    </div>
 
 </main>
 
